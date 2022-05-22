100c5e446470c9b6ecd42282b3e6cc782d3d9b60
mojhaid <mojahid319@gmail.com>
2022-05-23 05:48:41 +0600

    add footer design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/footer" element={<Footer />} />
         
       </Routes>
    </div>
---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import shape2 from '../../Assete/Image/shape-2.png'
+const Footer = () => {
+    return (
+        <div>
+            <footer  className="footer p-10 my-10" style={{
+
+background:`url(${shape2})`,
+backgroundSize:'cover',
+
+}}>
+<div>
+<span className="footer-title">Services</span> 
+<a className="link link-hover">Branding</a>
+<a className="link link-hover">Design</a>
+<a className="link link-hover">Marketing</a>
+<a className="link link-hover">Advertisement</a>
+</div> 
+<div>
+<span className="footer-title">Company</span> 
+<a className="link link-hover">About us</a>
+<a className="link link-hover">Contact</a>
+<a className="link link-hover">Jobs</a>
+<a className="link link-hover">Press kit</a>
+</div> 
+<div>
+<span className="footer-title">Legal</span> 
+<a className="link link-hover">Terms of use</a>
+<a className="link link-hover">Privacy policy</a>
+<a className="link link-hover">Cookie policy</a>
+</div>
+</footer>
+<div>
+<footer className="footer footer-center p-4 bg-base-300 text-base-content">
+  <div>
+    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+  </div>
+</footer>
+</div>
+        </div>
+    );
+};
+
+export default Footer;
