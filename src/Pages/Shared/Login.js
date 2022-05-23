4def50a57438ba448237d24ddb24fa8afc97db63
mojhaid <mojahid319@gmail.com>
2022-05-23 20:34:50 +0600

    add one sesction in hompage

--- src/Pages/Shared/Login.js
+++ src/Pages/Shared/Login.js
@@ -1,69 +1,50 @@
-import React from 'react';
-
+import React, { useRef } from "react";
+import auth from "../../firebase.init";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 const Login = () => {
-    return (
-        const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
-        const emailRef = useRef("");
-        const PasswordRef = useRef("");
-      
-        const handalSubmit = (event) => {
-          event.preventDefault();
-          const email = emailRef.current.value;
-          const password = PasswordRef.current.value;
-          console.log(email, password);
-        };
-      
-        const handalsignup=() =>{
-          navigate('/signup');
-        }
-      
-        const navigate = useNavigate();
-        if (guser) {
-          navigate("/");
-        }
-      
-        if (gloading) {
-          return <p>loading...</p>;
-        }
-        if (gerror) return <p>Error:{gerror.message}</p>;
-      
-        return (
-          <div>
-            <h3 className="text-center my-4"> Plese Login </h3>
-            <div className="w-25 mx-auto text-center">
-              <Form onSubmitCapture={handalSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control
-                    ref={emailRef}
-                    type="email"
-                    placeholder="Enter email"
-                  />
-                </Form.Group>
-      
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Control
-                    ref={PasswordRef}
-                    type="password"
-                    placeholder="Password"
-                  />
-                </Form.Group>
-      
-                <Button variant="primary" type="submit">
-                  Login
-                </Button>
-                <h5>or</h5>
-                <Button
-                  onClick={() => signInWithGoogle()}
-                  variant="primary"
-                  type="submit"
-                >
-                  Sign in with Google
-                </Button>
-              </Form>
-              <p className="mt-3">are you New? <Link to='/SignUp' onClick={handalsignup} className="text-danger mt-3 text-decoration-none">Plese Sign Up</Link> </p>
-            </div>
-          </div>
-    );
+  // for emai and pass
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const emaiRef = useRef("");
+  const passwordRef = useRef("");
+
+  // for google sign in
+  const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
+
+  return (
+    <div class="card w-96 bg-neutral text-neutral-content">
+      <div class="card-body items-center text-center">
+         <h1 className="text-3xl my-3">Plese Login</h1>
+        <div class="form-control w-full max-w-xs">
+          
+          <input
+            type="email"
+            placeholder="email"
+            class="input input-bordered w-full max-w-xs"required
+          />
+          
+        </div>
+        <div class="form-control w-full max-w-xs">
+          
+          <input
+            type="password"
+            placeholder="password"
+            class="input input-bordered w-full max-w-xs"required
+          />
+          
+        </div>
+
+       <button class="btn btn-primary">Login</button>
+        <div class="divider">OR</div>
+        <div class="card-actions justify-end">
+          <button onClick={() => signInWithGoogle()} class="btn btn-primary">
+            Sign in with Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
